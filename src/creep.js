2d66cb7c5644ebf3026d16e9a934ac5647766ed2
Adam Shumann <ashumann13@gmail.com>
2017-09-17 00:42:58 -0500

    TEST

--- src/creep.js
+++ src/creep.js
@@ -1,6 +1,8 @@
 const roles = {}
 class Creep {
   run (creep) {
+    creep.say('TEST')
+    return
     let role = creep.memory.role || 'scout'
     let rolec = this.tryLoadRole(role)
     if (rolec) {
