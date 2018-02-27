710fae5d73014a32b2220fb914c07e3f9aeb4d81
Adam Shumann <ashumann13@gmail.com>
2018-02-27 16:32:34 -0600

    CPU Tracking for harvester

--- src/legacy/HostileTracker.js
+++ src/legacy/HostileTracker.js
@@ -9,8 +9,12 @@ class HostileTracker {
     return Memory.hostileTracker
   }
   tick () {
+    let start = Game.cpu.getUsed()
     _.each(Game.rooms, room => this.analyze(room))
-    return
+    let end = Game.cpu.getUsed()
+    console.log('HostileTracker', (end - start).toFixed(3))
+  }
+  oldTick () {
     let { rooms } = this.mem
     let vis = new RoomVisual()
     let minx = 1000
@@ -120,45 +124,43 @@ class HostileTracker {
 
 module.exports = new HostileTracker()
 
-function getColorBasedOnPercentage(thePercentage)
-{
-    var hue = Math.floor((100 - thePercentage) * 120 / 100);  // go from green to red
-    var saturation = Math.abs(thePercentage - 50)/50;
-    return hsv2rgb(hue, saturation, 1);
+function getColorBasedOnPercentage (thePercentage) {
+  var hue = Math.floor((100 - thePercentage) * 120 / 100)  // go from green to red
+  var saturation = Math.abs(thePercentage - 50) / 50
+  return hsv2rgb(hue, saturation, 1)
 }
 
-var hsv2rgb = function(h, s, v) {
+var hsv2rgb = function (h, s, v) {
   // adapted from http://schinckel.net/2012/01/10/hsv-to-rgb-in-javascript/
-  var rgb, i, data = [];
+  var rgb, i, data = []
   if (s === 0) {
-    rgb = [v,v,v];
+    rgb = [v, v, v]
   } else {
-    h = h / 60;
-    i = Math.floor(h);
-    data = [v*(1-s), v*(1-s*(h-i)), v*(1-s*(1-(h-i)))];
-    switch(i) {
+    h = h / 60
+    i = Math.floor(h)
+    data = [v * (1 - s), v * (1 - s * (h - i)), v * (1 - s * (1 - (h - i)))]
+    switch (i) {
       case 0:
-        rgb = [v, data[2], data[0]];
-        break;
+        rgb = [v, data[2], data[0]]
+        break
       case 1:
-        rgb = [data[1], v, data[0]];
-        break;
+        rgb = [data[1], v, data[0]]
+        break
       case 2:
-        rgb = [data[0], v, data[2]];
-        break;
+        rgb = [data[0], v, data[2]]
+        break
       case 3:
-        rgb = [data[0], data[1], v];
-        break;
+        rgb = [data[0], data[1], v]
+        break
       case 4:
-        rgb = [data[2], data[0], v];
-        break;
+        rgb = [data[2], data[0], v]
+        break
       default:
-        rgb = [v, data[0], data[1]];
-        break;
+        rgb = [v, data[0], data[1]]
+        break
     }
   }
-  return '#' + rgb.map(function(x){
-    return ("0" + Math.round(x*255).toString(16)).slice(-2);
-  }).join('');
-};
-
+  return '#' + rgb.map(function (x) {
+    return ('0' + Math.round(x * 255).toString(16)).slice(-2)
+  }).join('')
+}
