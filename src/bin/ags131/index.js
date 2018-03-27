69be5c85a1143dd470b4f9533f0b44c833abddee
Adam Shumann <ashumann13@gmail.com>
2018-03-27 14:26:43 -0500

    Move testing processes to ags131 namespace

--- src/bin/ags131/index.js
+++ src/bin/ags131/index.js
@@ -1,9 +1,17 @@
+import IntTest from './IntTest'
+import ErrTest from './ErrTest'
+import ProcessTreeDump from './ProcessTreeDump'
+
 import { bundle as SleeperTest } from './SleeperTest'
 import { bundle as CronTest } from './CronTest'
 import { bundle as SpawnTest } from './SpawnTest'
 
 export const bundle = {
   install (processRegistry, extensionRegistry) {
+    processRegistry.register('ags131/intTest', IntTest)
+    processRegistry.register('ags131/errTest', ErrTest)
+    processRegistry.register('ags131/processTreeDump', ProcessTreeDump)
+
     SleeperTest.install(processRegistry, extensionRegistry)
     CronTest.install(processRegistry, extensionRegistry)
     SpawnTest.install(processRegistry, extensionRegistry)
--- src/bin/index.js
+++ src/bin/index.js
@@ -1,9 +1,5 @@
 import Init from './Init'
-import IntTest from './IntTest'
-import ErrTest from './ErrTest'
 import Cron from './Cron'
-
-import ProcessTreeDump from './ProcessTreeDump'
 
 import { bundle as POSISTest } from './POSISTest'
 import { bundle as ags131 } from './ags131'
@@ -13,10 +9,7 @@ import { bundle as ZeSwarm } from './ZeSwarm'
 export const bundle = {
   install (processRegistry, extensionRegistry) {
     processRegistry.register('init', Init)
-    processRegistry.register('intTest', IntTest)
-    processRegistry.register('errTest', ErrTest)
     processRegistry.register('cron', Cron)
-    processRegistry.register('processTreeDump', ProcessTreeDump)
 
     POSISTest.install(processRegistry, extensionRegistry)
     ags131.install(processRegistry, extensionRegistry)
