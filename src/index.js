d8fa3fa5da7dea968db40737862286f5923e07d5
Adam Shumann <ashumann13@gmail.com>
2018-04-03 08:32:13 -0500

    Force processes to use only exposed APIs

--- src/index.js
+++ src/index.js
@@ -42,8 +42,39 @@ extensionRegistry.register('memoryManager', new Proxy(memoryManager, {
 
 extensionRegistry.register('etc', etc)
 
+
 const kernel = new BaseKernel(processRegistry, extensionRegistry)
-extensionRegistry.register('baseKernel', kernel)
+function extChange(func, oldExt, newExt) {
+  const err = new Error(`'${func}' called on ${oldExt}! Use the '${newExt}' extension instead!`)
+  const msg = ErrorMapper.map(err)
+  kernel.log.warn(msg)
+}
+extensionRegistry.register('baseKernel', {
+  startProcess(imageName, startContext) {    return kernel.startProcess(imageName, startContext)  },
+  killProcess(pid) { return kernel.killProcess(pid)  },
+  getProcessById(pid) { return kernel.getProcessById(pid) },
+  setParent(pid, parentId) { return kernel.setParent(pid, parentId) },
+  sleep (time) { 
+    extChange('sleep','baseKernel','sleep')
+    return kernel.sleep(time)
+  },
+  setInterrupt (type, stage, key) { 
+    extChange('setInterrupt','baseKernel','interrupt')
+    return this.kernel.setInterrupt(type, stage, key) 
+  },
+  clearInterrupt (type, stage, key) { 
+    extChange('clearInterrupt','baseKernel','interrupt')
+    return this.kernel.clearInterrupt(type, stage, key) 
+  },
+  clearAllInterrupts () { 
+    extChange('clearAllInterrupts','baseKernel','interrupt')
+    return this.kernel.clearAllInterrupts() 
+  },
+  wait (type, stage, key) { 
+    extChange('wait','baseKernel','interrupt')
+    return this.kernel.wait(type, stage, key)
+  }
+})
 extensionRegistry.register('sleep', {
   sleep (time) { return kernel.sleep(time) }
 })
