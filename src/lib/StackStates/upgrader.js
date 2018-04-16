462d16410a4fc2f0158bad6351ef704dcf3add5f
Adam Shumann <ashumann13@gmail.com>
2018-04-16 15:41:20 -0500

    Upgrader limiters installed

--- src/lib/StackStates/upgrader.js
+++ src/lib/StackStates/upgrader.js
@@ -11,9 +11,8 @@ export default {
       this.push('moveToRoom', tgt)
       return this.runStack()
     }
-    let { room, pos } = this.creep
+    const { room, pos, room: { controller } } = this.creep
     if (this.creep.carry.energy) {
-      let controller = room.controller
       let upCnt = Math.ceil(this.creep.carry.energy / cache.work)
       this.push('repeat', upCnt, 'upgradeController', controller.id)
       this.push('moveInRange', controller.id, 3)
@@ -21,6 +20,10 @@ export default {
     } else {
       let tgt = room.storage || room.containers.find(c => c.store.energy)
       if (tgt) {
+        if (tgt === room.storage && tgt.store.energy < 10000 && controller.ticksToDowngrade < 10000) {
+          this.push('sleep', Game.time + 10)
+          return this.runStack()
+        }
         this.push('withdraw', tgt.id, C.RESOURCE_ENERGY)
         this.push('moveNear', tgt.id)
         return this.runStack()
